d4a2392402d5aa9eeae860a5cb87dda053aeb517
David Anderson <davidanderson42@pm.me>
2024-05-12 14:52:24 -0400

    Adding new folder

---
+++ Telnet_Network_Auto_2_R1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 13:43:39 2024
+
+@author: david
+"""
+
+import getpass
+port telnetlib
+
+HOST = "192.168.122.71"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"login: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
+
